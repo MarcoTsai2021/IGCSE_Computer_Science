bce3258ec1e9b23c51e82b837b7b71c62f51f933
arsalon amini <arsalonamini2020@gmail.com>
2021-12-17 09:47:33 +0800

    O(n) time example

---
+++ Algorithms/bigo.py
@@ -0,0 +1,17 @@
+#Big O notation (time/space complexity)
+#how we measure and compare efficiency of algorithms
+#Common Big-O values:
+#O(1) - constant space/time
+#O(n) - linear space/time
+#O(log n) - logarithmic space/time (merge and conquer) 
+#O(n^2) - quadratic space/time (nested loops)
+
+#O(n) time example 
+list = [1, 5, 6, 7, 8, 10, 11]
+for num in list:
+    print(num)
+
+
+
+
+
